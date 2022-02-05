8576a031cab3a78e93fdfa1bce3b42a2b818bde1
Devalent <info@devalent.com>
2022-02-05 17:18:14 +0000

    Update from https://github.com/Devalent/facial-recognition-service/commit/2afc07be77047dfe8835cf38b2fdc6536e9b5b6d

--- facial-recognition-app/_next/static/X0Fsk2hE3l7wOd1hDQRuS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/317-43b8cc37d0389fdc.js","static/css/8a4660f4d2ad5c48.css","static/chunks/pages/index-5769d0d075ae3e89.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
